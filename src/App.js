2f7d5d7e9ff8f6661113b43e84c5a6ce985ae0ec
weston-bailey <wdbailey87@gmail.com>
2023-01-10 17:04:06 -0800

    finish lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Movie from './Movie'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// fake data endpoint from our movie API
+const movies = [
+  {
+    title: 'The Fellowship of the Ring',
+    hours: 2,
+    minutes: 58
+  },
+  {
+    title: 'The Two Towers',
+    hours: 2,
+    minutes: 59
+  },
+  {
+    title: 'Return of the King',
+    hours: 3,
+    minutes: 21
+  },
+  {
+    title: 'The Hobbit: An Unexpected Journey',
+    hours: 2,
+    minutes: 49
+  }
+]
+
+export default class App extends Component {
+  render() {
+    // map the array of movies to movie components
+    // const movieComponents = movies.map((movie, idx) => {
+    //   return (
+    //     <Movie 
+    //       title={movie.title}
+    //       hours={movie.hours}
+    //       minutes={movie.minutes}
+    //       key={`movie-${idx}`}
+    //     />
+    //   )
+    // })
+
+    // this array will be rendered
+    const movieArray = []
+    movies.forEach((movie, idx) => {
+     movieArray.push(
+        <Movie 
+          title={movie.title}
+          hours={movie.hours}
+          minutes={movie.minutes}
+          key={`movie-${idx}`}
+        />
+      )
+    })
+
+
+
+    return (
+      <>
+        <h1>Lord of the Props</h1>
+
+        {movieArray}
+      </>
+    )
+  }
 }
-
-export default App;
---
+++ src/Movie.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Movie extends Component {
+    render() {
+        return (
+            <>
+                <h2>{this.props.title}</h2>
+                <p>{this.props.hours}h {this.props.minutes}min</p>
+            </>
+        )
+    }
+}
